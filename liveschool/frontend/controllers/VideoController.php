ee3be9491bf7f6555dfc08c8e71bb9b22b46a70e
weimade <wei@weimades-MacBook-Pro.local>
2016-07-29 20:49:21 +0800

    add detail,list

---
+++ liveschool/frontend/controllers/VideoController.php
@@ -0,0 +1,60 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
+/**
+ * Video controller
+ */
+class VideoController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+	/*
+	video list page
+	*/
+    public function actionList()
+    {
+        return $this->render('list');
+    }
+
+	/*
+	video detail page
+	*/
+    public function actionDetail()
+    {
+        return $this->render('detail');
+    }
+}
