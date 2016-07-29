c0a16611d5f50f962ede12afe56cfe4a565293f2
weimade <wei@weimades-MacBook-Pro.local>
2016-07-29 21:18:21 +0800

    udpate main.php

--- liveschool/frontend/views/layouts/main.php
+++ liveschool/frontend/views/layouts/main.php
@@ -19,6 +19,25 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/css/3-col-portfolio.css" rel="stylesheet">
+    <link href="/css/blog-post.css" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
     <?php $this->head() ?>
 </head>
 <body>
